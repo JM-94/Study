508aafb073c7bdb432991244464755875e69a1b5
JM-94 <fpskahs212@naver.com>
2021-06-01 16:02:11 +0900

    20210601

--- spring_work/mbat/src/main/java/com/org/mbat/HomeController.java
+++ spring_work/mbat/src/main/java/com/org/mbat/HomeController.java
@@ -1,9 +1,5 @@
 package com.org.mbat;
 
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.text.DateFormat;
-import java.util.Date;
 import java.util.Locale;
 
 import javax.sql.DataSource;
@@ -17,6 +13,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+
 @Controller
 public class HomeController {
 	
@@ -29,35 +26,8 @@ public class HomeController {
 	SqlSessionTemplate sst;
 	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		sst.insert("member.insert");
-		Connection conn = null;
-		try {
-			// 자료형에서 가져오기
-			conn = ds.getConnection();
-			System.out.println("연결성공");
-		}catch (Exception e) {
-			e.printStackTrace();
-		}
-		finally {
-			// 자료형으로 넣어주기
-			try {
-				conn.close();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
-		
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "home";
+	public String home(Model model) {
+		return "index";
 	}
 	
 }
