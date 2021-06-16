eef7757fd70ffc271bdf850fb25b24a4af4c6d48
JM-94 <fpskahs212@naver.com>
2021-06-16 16:39:43 +0900

    20210616

---
+++ C#_2/20210616/Test/Test/Test/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿
+namespace Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button_API = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dayDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.bNameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.rNameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dealBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dealBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button_API
+            // 
+            this.button_API.Location = new System.Drawing.Point(135, 38);
+            this.button_API.Name = "button_API";
+            this.button_API.Size = new System.Drawing.Size(75, 23);
+            this.button_API.TabIndex = 0;
+            this.button_API.Text = "API읽기";
+            this.button_API.UseVisualStyleBackColor = true;
+            this.button_API.Click += new System.EventHandler(this.button_API_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dayDataGridViewTextBoxColumn,
+            this.bNameDataGridViewTextBoxColumn,
+            this.rNameDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.dealBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 98);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(343, 324);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // dayDataGridViewTextBoxColumn
+            // 
+            this.dayDataGridViewTextBoxColumn.DataPropertyName = "Day";
+            this.dayDataGridViewTextBoxColumn.HeaderText = "Day";
+            this.dayDataGridViewTextBoxColumn.Name = "dayDataGridViewTextBoxColumn";
+            // 
+            // bNameDataGridViewTextBoxColumn
+            // 
+            this.bNameDataGridViewTextBoxColumn.DataPropertyName = "BName";
+            this.bNameDataGridViewTextBoxColumn.HeaderText = "BName";
+            this.bNameDataGridViewTextBoxColumn.Name = "bNameDataGridViewTextBoxColumn";
+            // 
+            // rNameDataGridViewTextBoxColumn
+            // 
+            this.rNameDataGridViewTextBoxColumn.DataPropertyName = "RName";
+            this.rNameDataGridViewTextBoxColumn.HeaderText = "RName";
+            this.rNameDataGridViewTextBoxColumn.Name = "rNameDataGridViewTextBoxColumn";
+            // 
+            // dealBindingSource
+            // 
+            this.dealBindingSource.DataSource = typeof(Test.Deal);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(369, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button_API);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dealBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_API;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dayDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn bNameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn rNameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.BindingSource dealBindingSource;
+    }
+}
+
---
+++ C#_2/20210616/Test/Test/Test2/Form1.cs
@@ -0,0 +1,72 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Test2
+{
+    public partial class Form1 : Form
+    {
+        const string FILENAME = "Coffees.json";
+        public List<Coffee> coffees = new List<Coffee>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_read_Click(object sender, EventArgs e)
+        {
+            string source = File.ReadAllText(FILENAME);
+            JObject jsonObjectCoffee = JObject.Parse(source);
+
+            coffees = (from item in jsonObjectCoffee["coffees"]["coffee"]
+                        select new Coffee()
+                        {
+                            menu = item["menu"].ToString().Replace("{", "").Replace("}", ""),
+                            price = int.Parse(item["price"].ToString().Replace("{", "").Replace("}", ""))
+                        }).ToList<Coffee>();
+            dataGridView1.DataSource = coffees;
+        }
+
+        private void button_write_Click(object sender, EventArgs e)
+        {
+            var jCoffeeArray = new JArray();
+            var jCoffeeObject = new JObject();
+
+            if (coffees.Count > 0)
+            {
+                foreach (var item in coffees)
+                {
+                    jCoffeeObject = new JObject();
+                    jCoffeeObject.Add("menu", item.menu);
+                    jCoffeeObject.Add("price", item.price);
+                    jCoffeeArray.Add(jCoffeeObject);
+                }
+            }
+
+            jCoffeeObject = new JObject();
+            jCoffeeObject.Add("menu", textBox_menu.Text);
+            jCoffeeObject.Add("price", textBox_price.Text);
+
+
+            jCoffeeArray.Add(jCoffeeObject);
+
+
+            var jCoffeeArray_object = new JObject();
+            jCoffeeArray_object.Add("coffee", jCoffeeArray);
+
+            var jCoffees_object = new JObject();
+            jCoffees_object.Add("coffees", jCoffeeArray_object);
+
+            File.WriteAllText(@"./Coffees.json", jCoffees_object.ToString());
+        }
+    }
+}
+
