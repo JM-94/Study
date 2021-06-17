e767f151dbc29ce84f5cdd0cd2d4876f850e5091
JM-94 <fpskahs212@naver.com>
2021-06-17 15:36:26 +0900

    20210617

---
+++ C#_2/20210617/practice_json/practice_json/Doctor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practice_json
+{
+    class Doctor
+    {
+        public string name { get; set; }
+        public string sabeon { get; set; }
+        
+        //private int age;
+        //public int Age { get => age; set => age = value; }
+
+    }
+}
